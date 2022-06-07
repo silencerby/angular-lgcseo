0cbbbee61e06c527fe9a89b616b2f63bba084755
Vitali Skleenkov <vitaly.skleenkov@united-security-providers.ch>
2022-06-07 15:23:45 +0300

    Retrieve shipping prices / Create a shipping component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Product } from './products';
 
@@ -9,7 +10,7 @@ export class CartService {
 
 	items: Product[] = [];
 
-	constructor() { }
+	constructor(private http: HttpClient) { }
 
 	addToCart(product: Product) {
 		this.items.push(product)
@@ -22,6 +23,10 @@ export class CartService {
 	clearCart() {
 		this.items = [];
 		return this.items;
+	}
+
+	getShippingPrices() {
+		return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
 	}
 
 }
