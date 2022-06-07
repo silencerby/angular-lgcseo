987c476c73c674212dd883205757d1d9d9c2d7e5
Vitali Skleenkov <vitaly.skleenkov@united-security-providers.ch>
2022-06-07 14:43:46 +0300

    Create the cart view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,29 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-    ])
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+	imports: [
+		BrowserModule,
+		ReactiveFormsModule,
+		RouterModule.forRoot([
+			{ path: '', component: ProductListComponent },
+			{ path: 'products/:productId', component: ProductDetailsComponent },
+			{ path: 'cart', component: CartComponent },
+		])
+	],
+	declarations: [
+		AppComponent,
+		TopBarComponent,
+		ProductListComponent,
+		ProductAlertsComponent,
+		ProductDetailsComponent,
+		CartComponent
+	],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
 
